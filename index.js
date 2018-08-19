06ed2069f54adafecb5816b05991cc385a5f58ea
Bushra Masud <bushra.masud111@gmail.com>
2018-08-19 07:31:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,10 +14,9 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   if(string.toLowerCase()){
     return "I can't hear you!"
-  }
-}
-function sayHiToGrandma(string){
-  if(string.toUpperCase()){
+  }else if (string.toUpperCase()) {
     return "YES INDEED!"
+    
   }
 }
+
